6633615d3b4ef3e2eb29bed21dc9acb6c553aafe
Austin Gabe <57731755+thinkblue721@users.noreply.github.com>
2020-08-09 20:24:09 -0700

    Full app functionality

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,8 +1,8 @@
 const router = require("express").Router();
 const Workout = require("../models/Workout");
 
-router.post("/api/workouts", ({ body }, res) => {
-    Workout.create(body)
+router.post("/api/workouts", (req, res) => {
+    Workout.create(req.body)
         .then(dbWorkout => {
             res.json(dbWorkout);
         })
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -13,4 +13,8 @@ router.get("/exercise?", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/stats.html"));
 })
 
+router.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/stats.html"))
+})
+
 module.exports = router;
